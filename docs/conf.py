0ca02dce333b641678b352004f59276de0412153
Lawrence Spector <lawrence.spector@gmail.com>
2020-03-25 13:52:43 -0400

    Update how doxygen is launched, to be more friendly to markdown

--- docs/conf.py
+++ docs/conf.py
@@ -3,5 +3,5 @@ import subprocess, os
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    subprocess.call('pwd; pushd .. && doxygen .doxygen-config; popd', shell=True)
+    subprocess.call('pwd; cd ../doxygen && doxygen ../.doxygen-config; cd ../docs', shell=True)
 
