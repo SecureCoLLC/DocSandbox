2a41c4667b9544d21b0030fb7c857e4db595c91f
Lawrence Spector <lawrence.spector@gmail.com>
2020-03-25 13:48:33 -0400

    Adjust command used to launch doxygen

--- docs/conf.py
+++ docs/conf.py
@@ -3,5 +3,5 @@ import subprocess, os
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    subprocess.call('cd ../doxygen; doxygen .doxygen-config', shell=True)
+    subprocess.call('pwd; pushd .. && doxygen .doxygen-config; popd', shell=True)
 
