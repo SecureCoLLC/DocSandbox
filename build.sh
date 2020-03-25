28c9f325ef2bb4360119e38188e9f4372bbc2c31
Lawrence Spector <lawrence.spector@gmail.com>
2020-03-25 13:17:11 -0400

    Build go code as part of build

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 mkdir -p bin/cpp
+mkdir -p bin
 
 clang++ -std=c++17 -o bin/cpp/Square.o -c cpp/Square.cpp
 clang++ -std=c++17 -o bin/cpp/Cube.o -c cpp/Cube.cpp
@@ -10,3 +11,9 @@ clang++ -std=c++17 -o bin/cpp/main.o -c cpp/main.cpp
 clang++ -std=c++17 -o bin/ShapesCpp bin/cpp/main.o bin/cpp/Cube.o bin/cpp/Square.o
 
 bin/ShapesCpp
+
+pushd go
+go build -o bin/ShapesGo .
+popd
+
+
