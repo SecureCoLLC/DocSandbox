a7ce5f5ba148ac242950c7464e6bea276bba5a81
Lawrence Spector <lawrence.spector@gmail.com>
2020-03-25 14:13:38 -0400

    Make doxygen output more verbose

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,28 @@
-import subprocess, os
+import subprocess, os, sys
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+class Doxygen:
+    def run(self, ):
+        """Run the doxygen make command in the designated folder"""
 
-if read_the_docs_build:
-    subprocess.call('cd .. && scripts/doxygen.sh', shell=True)
+        try:
+            retcode = subprocess.call('cd .. && scripts/doxygen.sh', shell=True)
+            if retcode < 0:
+                sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
+        except OSError as e:
+            sys.stderr.write("doxygen execution failed: %s" % e)
 
-master_doc = '../doxygen/index'
+
+    def generate(self, app):
+        """Run the doxygen make commands if we're on the ReadTheDocs server"""
+
+        read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+        if read_the_docs_build:
+            run_doxygen()
+
+def setup(app):
+    doxygen = Doxygen()
+    master_doc = '../doxygen/index'
+
+    # Add hook for building doxygen xml when needed
+    app.connect("builder-inited", doxygen.generate)
