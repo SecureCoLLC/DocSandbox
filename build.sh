4dd06f320498087aabe204fcc3dac56439f6f101
Lawrence Spector <lawrence.spector@gmail.com>
2020-03-25 13:28:01 -0400

    Update logic to build and run both go and C++

--- build.sh
+++ build.sh
@@ -5,15 +5,20 @@ set -euo pipefail
 mkdir -p bin/cpp
 mkdir -p bin
 
+echo "### Building ShapesCpp"
 clang++ -std=c++17 -o bin/cpp/Square.o -c cpp/Square.cpp
 clang++ -std=c++17 -o bin/cpp/Cube.o -c cpp/Cube.cpp
 clang++ -std=c++17 -o bin/cpp/main.o -c cpp/main.cpp
 clang++ -std=c++17 -o bin/ShapesCpp bin/cpp/main.o bin/cpp/Cube.o bin/cpp/Square.o
 
+echo "### Running ShapesCpp"
 bin/ShapesCpp
 
+echo "### Building ShapesGo"
 pushd go
-go build -o bin/ShapesGo .
+go build -o ../bin/ShapesGo .
 popd
 
+echo "### Running ShapesGo"
+./bin/ShapesGo
 
