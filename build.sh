ef742772a51bd4bc2e0d20bb337cafa508107462
Lawrence Spector <lawrence.spector@gmail.com>
2020-03-25 12:54:49 -0400

    Add build for shapes C++

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-clang++ -o bin/Square.o -c cpp/Square.cpp
-clang++ -o bin/Cube.o -c cpp/Cube.cpp
+mkdir -p bin/cpp
 
+clang++ -std=c++17 -o bin/cpp/Square.o -c cpp/Square.cpp
+clang++ -std=c++17 -o bin/cpp/Cube.o -c cpp/Cube.cpp
+clang++ -std=c++17 -o bin/cpp/main.o -c cpp/main.cpp
+clang++ -std=c++17 -o bin/ShapesCpp bin/cpp/main.o bin/cpp/Cube.o bin/cpp/Square.o
